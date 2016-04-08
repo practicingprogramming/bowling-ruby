0dfca0f634666a107b56fd5fb9975d27604094dc
practicingprogramming <practicingprogramming@gmail.com>
2016-04-07 22:26:57 -0700

    Extract strike? and spare?

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -17,13 +17,13 @@ module Bowling
       frame_roll_index = 0
       @rolls.each_with_index do |roll, roll_index|
         @frames[frame_index] += roll
-        if frame_roll_index == 1 && @frames[frame_index] == 10 && frame_index < FRAME_NUMBER - 1
+        if spare?(frame_index, frame_roll_index)
           @frames[frame_index] += @rolls[roll_index + 1]
           frame_index += 1
           frame_roll_index = 0
           next
         end
-        if frame_roll_index == 0 && @frames[frame_index] == 10 && frame_index < FRAME_NUMBER - 1
+        if strike?(frame_index, frame_roll_index)
           @frames[frame_index] += @rolls[roll_index + 1]
           @frames[frame_index] += @rolls[roll_index + 2]
           frame_index += 1
@@ -38,6 +38,16 @@ module Bowling
         end
       end
       @frames.inject(0, :+)
+    end
+
+    private
+
+    def spare?(frame_index, frame_roll_index)
+      frame_roll_index == 1 && @frames[frame_index] == 10 && frame_index < FRAME_NUMBER - 1
+    end
+
+    def strike?(frame_index, frame_roll_index)
+      frame_roll_index == 0 && @frames[frame_index] == 10 && frame_index < FRAME_NUMBER - 1
     end
   end
 end
